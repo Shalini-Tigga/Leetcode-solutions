146a22e6688f986b3b33fce58f6feb7653050ae5
Shalini Tigga <116837673+Shalini-Tigga@users.noreply.github.com>
2025-06-07 23:24:20 +0530

    Create 3170-Lexicographically-Minimum-String-After-Removing-Stars.cpp

---
+++ 3170-Lexicographically-Minimum-String-After-Removing-Stars.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        vector<vector<int>> lookup(26);
+        for (int i = 0; i < size(s); ++i) {
+            if (s[i] != '*') {
+                lookup[s[i] - 'a'].emplace_back(i);
+                continue;
+            }
+            for (auto& stk : lookup) {
+                if (empty(stk)) {
+                    continue;
+                }
+                s[stk.back()] = '*';
+                stk.pop_back();
+                break;
+            }
+        }
+        s.erase(remove(begin(s), end(s), '*'), end(s));
+        return s;
+    }
+};
